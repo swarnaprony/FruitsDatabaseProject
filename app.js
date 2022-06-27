52332527f66f79a941db346c24b43e6700b0e961
Swarna Sarker <swarnaprony81@gmail.com>
2022-06-27 16:15:12 +0200

    Create mongoose database and connect it insert data into it

--- app.js
+++ app.js
@@ -1,43 +1,21 @@
-const { MongoClient } = require("mongodb");
-const assert = require("assert");
-// Replace the uri string with your MongoDB deployment's connection string.
-const uri =
-  "mongodb://localhost:27017";
-const client = new MongoClient(uri);
-async function executeInMongo() {
-  try {
-    await client.connect();
-    console.log("Connected to the Database");
-    const database = client.db('shopDB');
-    const fruits = database.collection('fruits');
+// Using Node.js `require()`
+const mongoose = require('mongoose');
 
-    // // Query for a movie that has the title 'Back to the Future'
-    // const query = { title: 'Back to the Future' };
-    // const movie = await movies.findOne(query);
-
-    // assert.equal(movie.title, 'Back to the Future');
-
-    await database.collection("fruits").insertMany([
-      {
-        name: "Apple",
-        score: 8,
-        reviiew: "Great Fruit"
-      }, {
-        name: "Orange",
-        score: 6,
-        reviiew: "Sour"
-      }, {
-        name: "Banana",
-        score: 9,
-        reviiew: "Sweet"
-      }
-    ]);
-
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
+mongoose.connect("mongodb://localhost:27017/fruitsDB");
 
 
-    executeInMongo();
+const fruitSchema = new mongoose.Schema ({
+  name: String,
+  rating: Number,
+  review: String
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit ({
+  name: "Apple",
+  rating: 7,
+  review: "Soild as a fruit."
+});
+
+fruit.save();
