d12eff6d04a34c8dd5d1ca5f7d779db084f6394e
Swarna Sarker <swarnaprony81@gmail.com>
2022-06-27 17:22:02 +0200

    Update app.js to insertmany data in fruits collection

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/fruitsDB");
 
 
+const personSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  age: Number
+});
+
+
 const fruitSchema = new mongoose.Schema ({
   name: String,
   rating: Number,
@@ -18,4 +25,37 @@ const fruit = new Fruit ({
   review: "Soild as a fruit."
 });
 
-fruit.save();
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  firstName: "Asif",
+  lastName: "Hossain",
+  age: 30
+});
+
+//person.save();
+
+const kiwi = new Fruit({
+  name: "Kiwi",
+  score: 5,
+  review: "Not too tasty"
+});
+
+const orange = new Fruit({
+  name: "Orange",
+  score: 6,
+  review: "Sour"
+});
+
+const banana = new Fruit({
+  name: "Banana",
+  score: 9,
+  review: "Sweet"
+});
+
+
+Fruit.insertMany([kiwi, orange, banana]).then (function(){
+    console.log("Successfully saved all the fruits to fruitsDB")
+  }).catch (function(error){
+    console.log(error)
+  });
