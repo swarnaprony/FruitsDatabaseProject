2938a141f3fddcd60609a71ab957f5e438400317
Swarna Sarker <swarnaprony81@gmail.com>
2022-06-27 19:19:07 +0200

    "Find json object from documents"

--- practice.js
+++ practice.js
@@ -1,16 +1,55 @@
+
+
+
+
+
 const { MongoClient } = require("mongodb");
-// Replace the uri string with your MongoDB deployment's connection string.
-const uri =
+const assert = require('assert');
+
+// Replace the url string with your MongoDB deployment's connection string.
+const url =
   "mongodb://localhost:27017";
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    await client.connect();
-    const database = client.db('FruitsDB');
-    console.log("Coonnecte to the Database");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+const dbName = "fruitDB"
+
+const client = new MongoClient(url, { userNewUrlParser: true });
+
+  client.connect(err);
+  assert.equal(null, err);
+  console.log("Connected Successfully to server");
+
+  const db = client.db(dbName);
+
+insertDocuments(db, function () {
+  client.close();
+});
+
+
+const insertDocuments = function (db, callback) {
+  // Get the documents collection
+  const collection = db.collection("fruits");
+  // Insert some documents
+  collection.insertMany([
+    {
+      name: "Apple",
+      score: 8,
+      reviiew: "Great Fruit"
+    }, {
+      name: "Orange",
+      score: 6,
+      reviiew: "Sour"
+    }, {
+      name: "Banana",
+      score: 9,
+      reviiew: "Sweet"
+    }
+
+
+  ], function (err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+};
